c28da7f3b9cd98f600c2b6b6f3960507222fd2fc
Raj-Govekar <raj.2504696101@vcet.edu.in>
2026-08-09 03:37:10 +0530

    Deploy minimal working app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,11 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { AuthProvider } from "@/context/AuthContext";
-import { Toaster } from "sonner";
-import Login from "@/pages/Login";
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <AuthProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/" element={<Navigate to="/login" />} />
-          </Routes>
-        </BrowserRouter>
-        <Toaster position="top-right" />
-      </AuthProvider>
+      <h1>🚀 The Stacks - Coming Soon!</h1>
+      <p>Your library management system is being deployed...</p>
     </div>
   );
 }
