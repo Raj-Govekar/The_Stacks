deddb5707d5d043d978aa5d816b835e3293092c5
Raj Govekar <raj.2504696101@vcet.edu.in>
2026-08-09 01:52:50 +0530

    Update Libraries.js

--- frontend/src/pages/admin/Libraries.js
+++ frontend/src/pages/admin/Libraries.js
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
-import api,{formatApiErrorDetail} from "@/lib/api";
-import {Button} from "@/components/ui/button"; import {Input} from "@/components/ui/input"; import {Label} from "@/components/ui/label";
-import {Dialog,DialogContent,DialogHeader,DialogTitle,DialogFooter} from "@/components/ui/dialog";
-import {AlertDialog,AlertDialogAction,AlertDialogCancel,AlertDialogContent,AlertDialogDescription,AlertDialogFooter,AlertDialogHeader,AlertDialogTitle} from "@/components/ui/alert-dialog";
+import api, { formatApiErrorDetail } from "../../lib/api";
+import { Button } from "../../components/ui/button";
+import { Input } from "../../components/ui/input";
+import { Label } from "../../components/ui/label";
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogFooter } from "../../components/ui/dialog";
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle } from "../../components/ui/alert-dialog";
 import {Plus,Pencil,Trash2,MapPin,Phone,Mail,Loader2} from "lucide-react"; import {toast} from "sonner";
 const EMPTY={name:"",address:"",phone:"",email:""};
 export default function Libraries(){
